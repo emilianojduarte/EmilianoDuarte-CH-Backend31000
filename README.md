9f9cc7bb1f3eced8b76a48ddca464a7eded413fd
Emiliano Duarte <emilianojduarte@gmail.com>
2022-06-27 22:44:29 -0300

    Clase 12
    Entrega de la clase 12
    Se configuró el proyecto para realizar lo solicitado.
    
    Se eliminó todo lo que no era necesario y se redujo al minimo.

--- README.md
+++ README.md
@@ -3,14 +3,3 @@
 ## CoderHuse-Backend 31000
  Repositorio del curso de Backend en CoderHouse - Comision 31000
 
-
-### Entrega - Clase 10
-Reemplazar en el package.json la linea 7 según el motor que se quiera utilizar.
-De la siguiente manera:
-
-1 - "start": "nodemon ./src-handlebars/server.js"
-
-2 - "start": "nodemon ./src-pug/server.js"
-
-3 - "start": "nodemon ./src-ejs/server.js"
-  
--- src/server.js
+++ src/server.js
@@ -1,128 +1,120 @@
-//listado de productos
+/////DECLARACIONES
+const express = require('express');
+const app = express();
+const puerto = 8080;
+const path = require('path');
+const expressServer = app.listen(puerto, () => {
+    try{
+        console.log(`El servidor está escuchando el puerto: ${puerto}`)
+    }
+    catch(error){
+        console.log("Ocurrió el siguiente error al iniciar: ", error);
+    }
+});
+//io server
+const { Server: IOServer } = require('socket.io');
+const io = new IOServer(expressServer);
+//array
+const msgArray = [];
 const productos = [
     {
         "id": 1,
-        "type": "ram",
         "url": "https://cdn1.iconfinder.com/data/icons/_dock_icons___by_adrenn/128/kingston.png",
-        "enviogratis": "0",
-        "recomendado": "1",
         "price": 3500,
         "description": "1x4GB Kingston ValueRAM DDR3"
     },
     {
         "id": 2,
-        "type": "ram",
         "url": "https://cdn1.iconfinder.com/data/icons/_dock_icons___by_adrenn/128/kingston.png",
-        "enviogratis": "1",
-        "recomendado": "0",
         "price": 5499,
         "description": "1x8GB Kingston Fury DDR4"
     },
     {
         "id": 3,
-        "type": "ram",
         "url": "https://cdn1.iconfinder.com/data/icons/_dock_icons___by_adrenn/128/kingston.png",
-        "enviogratis": "1",
-        "recomendado": "1",
         "price": 16108,
         "description": "2x16GB Kingston Fury Beast DDR4"
     },
     {
         "id": 4,
-        "type": "cpuintel",
         "url": "https://cdn2.iconfinder.com/data/icons/metro-ui-dock/128/Intel.png",
-        "enviogratis": "1",
-        "recomendado": "1",
         "price": 26950,
         "description": "10ma Generación Intel i3"
     },
     {
         "id": 5,
-        "type": "cpuintel",
         "url": "https://cdn2.iconfinder.com/data/icons/metro-ui-dock/128/Intel.png",
-        "enviogratis": "1",
-        "recomendado": "1",
         "price": 24999,
         "description": "10ma Generación Intel i5"
     },
     {
         "id": 6,
-        "type": "cpuintel",
         "url": "https://cdn2.iconfinder.com/data/icons/metro-ui-dock/128/Intel.png",
-        "enviogratis": "0",
-        "recomendado": "1",
         "price": 43399,
         "description": "10ma Generación Intel i7"
     },
     {
         "id": 7,
-        "type": "cpuintel",
         "url": "https://cdn2.iconfinder.com/data/icons/metro-ui-dock/128/Intel.png",
-        "enviogratis": "1",
-        "recomendado": "0",
         "price": 55900,
         "description": "10ma Generación Intel i9"
     },
     {
         "id": 8,
-        "type": "cpuamd",
         "url": "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/15_Amd_logo_logos-128.png",
-        "enviogratis": "1",
-        "recomendado": "0",
         "price": 32662,
         "description": "3ra Generación Ryzen 3"
     },
     {
         "id": 9,
-        "type": "cpuamd",
         "url": "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/15_Amd_logo_logos-128.png",
-        "enviogratis": "0",
-        "recomendado": "1",
         "price": 41999,
         "description": "3ra Generación Ryzen 5"
     },
     {
         "id": 10,
-        "type": "cpuamd",
         "url": "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/15_Amd_logo_logos-128.png",
-        "enviogratis": "1",
-        "recomendado": "1",
         "price": 61722,
         "description": "3ra Generación Ryzen 7"
     },
     {
         "id": 11,
-        "type": "cpuamd",
         "url": "https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/15_Amd_logo_logos-128.png",
-        "enviogratis": "1",
-        "recomendado": "0",
         "price": 77400,
         "description": "3ra Generación Ryzen 9"
     }
 ];
-//funciones
-const getProductos = (req, res) => {
-    res.render('pages/products', {productos});
-}
-const addProducto = (req, res) => {
-    const { type, url, enviogratis, recomendado, price, description } = req.body
-    let ultimo = productos.length - 1;
-    let id = productos[ultimo].id + 1;
-    productos.push({ id, type, url, enviogratis, recomendado, price, description});
-    res.redirect('/');
-}
+//contenedor de archivo
+const Contenedor = require('./contenedor.js');
+dbChats = new Contenedor;
+//--------------------------------------------//
 
-//Pendiente de implementar
-// const getProducto = (req, res) => {
-//     let encontrado = productos.find(producto => producto.id == req.params.id);
-//     let resultado;
-//     if(encontrado){
-//         resultado = encontrado;
-//     } else {
-//         resultado = {error: 'El producto no existe'};
-//     }
-//     res.json(resultado);
-// }
+/////APLICACION
 
-//export
-module.exports = {getProductos, addProducto};
+//conf para acceder al body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//rutas estáticas - public
+app.use(express.static(path.join(__dirname, '../public')));
+
+//io sockets
+io.on('connection', socket => {
+    console.log('Se conectó el cliente con id: ', socket.id);
+    //productos
+    socket.emit('server:products', productos);
+    socket.on('client:product', productoInfo => {
+        productos.push(productoInfo);
+        io.emit('server:products', productos);
+    })
+    //mensajes
+    socket.emit('server:msgs', msgArray);
+    socket.on('client:msg', msgInfo => {
+        msgArray.push(msgInfo);
+        dbChats.save(msgInfo);
+        io.emit('server:msgs', msgArray)
+    })
+})
+
+//file manager del chat
+
