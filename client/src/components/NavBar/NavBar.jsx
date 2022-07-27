d5363ab8c13059398dc5b95ec902e61ab25ec7e1
Emiliano Duarte <emilianojduarte@gmail.com>
2022-07-27 16:17:09 -0300

    Pre-entrega final 2
    Limpieza de componentes que ya no se utilizan.
    Formateo de todo el proyecto con prettier.
    
    Adicionalmente se agregaron algunas cosas en el front .
    
    FIX

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,30 +2,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 //Estilos
-import './NavBar.css';
+import "./NavBar.css";
 
-function NavBar () {
-    return(
-        <header>
-            <div>
-                <Link to={'/'}>
-                    NombreDeLaTienda
-                </Link>
-            </div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to={'/'}> Home </Link>
-                    </li>
-                    <li>
-                        <Link to={'/productos'}> Productos </Link>
-                    </li>
-                    <li>Carrito</li>
-                    <li>Login</li>
-                </ul>
-            </nav>
-        </header>
-    )
+function NavBar() {
+  return (
+    <header>
+      <div>
+        <Link to={"/"}>NombreDeLaTienda</Link>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to={"/"}> Home </Link>
+          </li>
+          <li>
+            <Link to={"/productos"}> Productos </Link>
+          </li>
+          <li>Carrito</li>
+          <li>Login</li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default NavBar;
